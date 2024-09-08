c482b233682a63b401a41a4755d72526c20a869c
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-09-08 21:59:27 +0200

    Initial commit

---
+++ src/main/java/com/example/ProjectConnectedDatabase/ProjectConnectedDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectConnectedDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectConnectedDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectConnectedDatabaseApplication.class, args);
+	}
+
+}
