07aaec86d0d9f57a554023e8f5b255e757f5dd35
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-09-08 22:45:18 +0200

    JDBC connection created - successfully connected

--- src/main/java/com/example/ProjectConnectedDatabase/ProjectConnectedDatabaseApplication.java
+++ src/main/java/com/example/ProjectConnectedDatabase/ProjectConnectedDatabaseApplication.java
@@ -3,11 +3,26 @@ package com.example.ProjectConnectedDatabase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class ProjectConnectedDatabaseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectConnectedDatabaseApplication.class, args);
+
+			String url = "jdbc:mysql://localhost:3306/school";
+			String user = "root";
+			String password = "Teodorek1810";
+
+			try (Connection connection = DriverManager.getConnection(url, user, password)) {
+				System.out.println("Connection established successfully.");
+			} catch (SQLException e) {
+				System.err.println("SQL Exception occurred.");
+				e.printStackTrace();
+			}
+		}
 	}
 
-}
